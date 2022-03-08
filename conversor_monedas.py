4ab2da51eb59207319e2d41d8e2e8ca8e11c78d9
peterfunk57 <72843404+peterfunk57@users.noreply.github.com>
2022-03-07 22:49:19 -0600

    Se agrega script en python para convertir monedas a dollares

---
+++ conversor_monedas.py
@@ -0,0 +1,51 @@
+menu = """
+Bienvenido al Conversor de monedas ❤🐱‍💻
+Elige una opción porfavor
+
+1 - Pesos Colombianos
+2 - Pesos Mexicanos
+3 - Pesos Argentinos
+
+Elige una opnción:"""
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input ("Teclea la cantidad de Pesos Colombianos: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    valor_dolar = float(valor_dolar)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $ " + dolares + " dolares")
+elif opcion == 2:
+    pesos = input ("Teclea la cantidad de Pesos Mexicanos: ")
+    pesos = float(pesos)
+    valor_dolar = 19.80
+    valor_dolar = float(valor_dolar)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $ " + dolares + " dolares")
+elif opcion == 3:
+    pesos = input ("Teclea la cantidad de Pesos Argentinos: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    valor_dolar = float(valor_dolar)
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $ " + dolares + " dolares")
+else:
+    print("Elige una opcion valida Menzo")
+
+#cantidad_dolares = input("Teclea la cantidad de dollares ")
+#cantidad_dolares = float(cantidad_dolares)
+#cantidad_pesos = cantidad_dolares * valor_dolar
+#cantidad_pesos = round(cantidad_pesos, 2)
+#cantidad_pesos = str(cantidad_pesos)
+#print("Tienes $" + cantidad_pesos +  " pesos")
+
+
+
